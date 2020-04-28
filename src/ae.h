52fbf572703af14979777ede128a7a0b25296a5e
hongyi <1103009779@qq.com>
2020-04-28 17:17:33 +0800

    add ae_epoll.c annotation

--- src/ae.h
+++ src/ae.h
@@ -103,7 +103,7 @@ typedef struct aeEventLoop {//事件处理器
     aeFiredEvent *fired; /* Fired events *///待处理的事件编号和类型
     aeTimeEvent *timeEventHead;//注册的时间事件，基于链表实现
     int stop;//是否停止处理
-    void *apidata; /* This is used for polling API specific data */
+    void *apidata; /* This is used for polling API specific data *///主要是与epoll机制下的socket描述符列表交互
     aeBeforeSleepProc *beforesleep;
     aeBeforeSleepProc *aftersleep;
     int flags;
--- src/ae_epoll.c
+++ src/ae_epoll.c
@@ -31,22 +31,23 @@
 
 #include <sys/epoll.h>
 
-typedef struct aeApiState {
-    int epfd;
-    struct epoll_event *events;
+typedef struct aeApiState {//事件结构体
+    int epfd;//用于监听socket的文件描述符
+    struct epoll_event *events;//网络socket链表
 } aeApiState;
 
-static int aeApiCreate(aeEventLoop *eventLoop) {
-    aeApiState *state = zmalloc(sizeof(aeApiState));
+static int aeApiCreate(aeEventLoop *eventLoop) {//传入事件处理器
+    aeApiState *state = zmalloc(sizeof(aeApiState));//分配内存
 
     if (!state) return -1;
-    state->events = zmalloc(sizeof(struct epoll_event)*eventLoop->setsize);
-    if (!state->events) {
+    state->events = zmalloc(sizeof(struct epoll_event)*eventLoop->setsize);//基于事件处理器里的容量为events分配内存
+    if (!state->events) {//如果内存分配失败，记得释放state内存
         zfree(state);
         return -1;
     }
+    //基于epoll机制，创建1024长度的监听描述符链表
     state->epfd = epoll_create(1024); /* 1024 is just a hint for the kernel */
-    if (state->epfd == -1) {
+    if (state->epfd == -1) {//创建失败
         zfree(state->events);
         zfree(state);
         return -1;
@@ -55,17 +56,17 @@ static int aeApiCreate(aeEventLoop *eventLoop) {
     return 0;
 }
 
-static int aeApiResize(aeEventLoop *eventLoop, int setsize) {
+static int aeApiResize(aeEventLoop *eventLoop, int setsize) {//调整事件处理器下的apidata属性大小
     aeApiState *state = eventLoop->apidata;
 
     state->events = zrealloc(state->events, sizeof(struct epoll_event)*setsize);
     return 0;
 }
 
-static void aeApiFree(aeEventLoop *eventLoop) {
+static void aeApiFree(aeEventLoop *eventLoop) {//释放内存
     aeApiState *state = eventLoop->apidata;
 
-    close(state->epfd);
+    close(state->epfd);//关闭用来监听的文件描述符
     zfree(state->events);
     zfree(state);
 }
@@ -76,17 +77,19 @@ static int aeApiAddEvent(aeEventLoop *eventLoop, int fd, int mask) {
     /* If the fd was already monitored for some event, we need a MOD
      * operation. Otherwise we need an ADD operation. */
     int op = eventLoop->events[fd].mask == AE_NONE ?
-            EPOLL_CTL_ADD : EPOLL_CTL_MOD;
+            EPOLL_CTL_ADD : EPOLL_CTL_MOD;//EPOLL_CTL_ADD：注冊新的fd到epfd中；
+                                          //EPOLL_CTL_MOD：改动已经注冊的fd的监听事件；
 
     ee.events = 0;
     mask |= eventLoop->events[fd].mask; /* Merge old events */
-    if (mask & AE_READABLE) ee.events |= EPOLLIN;
-    if (mask & AE_WRITABLE) ee.events |= EPOLLOUT;
+    if (mask & AE_READABLE) ee.events |= EPOLLIN;//可读
+    if (mask & AE_WRITABLE) ee.events |= EPOLLOUT;//可写
     ee.data.fd = fd;
+    //进行事件注册
     if (epoll_ctl(state->epfd,op,fd,&ee) == -1) return -1;
     return 0;
 }
-
+//事件删除
 static void aeApiDelEvent(aeEventLoop *eventLoop, int fd, int delmask) {
     aeApiState *state = eventLoop->apidata;
     struct epoll_event ee = {0}; /* avoid valgrind warning */
@@ -109,22 +112,24 @@ static int aeApiPoll(aeEventLoop *eventLoop, struct timeval *tvp) {
     aeApiState *state = eventLoop->apidata;
     int retval, numevents = 0;
 
+    //监听变化，等待事件产生，返回需要处理的事件数量，同时把处理事件加入events内，
+    //能够遍历events来进行事件处理
     retval = epoll_wait(state->epfd,state->events,eventLoop->setsize,
             tvp ? (tvp->tv_sec*1000 + tvp->tv_usec/1000) : -1);
-    if (retval > 0) {
+    if (retval > 0) {//发现有动静
         int j;
 
         numevents = retval;
-        for (j = 0; j < numevents; j++) {
+        for (j = 0; j < numevents; j++) {//开始遍历发生动静的socket
             int mask = 0;
-            struct epoll_event *e = state->events+j;
+            struct epoll_event *e = state->events+j;//依次获取事件
 
             if (e->events & EPOLLIN) mask |= AE_READABLE;
             if (e->events & EPOLLOUT) mask |= AE_WRITABLE;
             if (e->events & EPOLLERR) mask |= AE_WRITABLE|AE_READABLE;
             if (e->events & EPOLLHUP) mask |= AE_WRITABLE|AE_READABLE;
-            eventLoop->fired[j].fd = e->data.fd;
-            eventLoop->fired[j].mask = mask;
+            eventLoop->fired[j].fd = e->data.fd;//更新事件处理器里的fired数组
+            eventLoop->fired[j].mask = mask;//事件类型
         }
     }
     return numevents;
